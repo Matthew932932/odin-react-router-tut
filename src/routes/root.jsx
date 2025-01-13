b232bb24f0689f0116bdf17bda3f895481a5c7dc
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-01-13 16:35:44 +1100

    half way through tutorial

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,21 @@
+import { Outlet, 
+          Link, 
+          useLoaderData,
+          Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts.js";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
+
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -22,22 +39,42 @@ export default function Root() {
                 aria-live="polite"
               ></div>
             </form>
-            <form method="post">
+            <Form method="post">
               <button type="submit">New</button>
-            </form>
+            </Form>
           </div>
           <nav>
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+            {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
             </ul>
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail">
+          <Outlet />
+        </div>
       </>
     );
   }
+
+   
