4d9b6df383fb181d249d5ab08a3dd88cac68b900
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-01-14 12:17:15 +1100

    tutorial complete

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,17 @@ import './index.css'
 import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 import Root, {
   loader as rootLoader,
   action as rootAction,
 } from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -23,11 +29,47 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-    ],
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          },
+        ],
+      }
+    ]
+    // children: [
+    //   { index: true, element: <Index /> },
+    //   {
+    //     path: "contacts/:contactId",
+    //     element: <Contact />,
+    //     loader: contactLoader,
+    //     action: contactAction,
+    //   },
+    //   {
+    //     path: "contacts/:contactId/edit",
+    //     element: <EditContact />,
+    //     loader: contactLoader,
+    //     action: editAction,
+    //   },
+    //   {
+    //     path: "contacts/:contactId/destroy",
+    //     action: destroyAction,
+    //   },
+    // ],
   },
 ]);
 
